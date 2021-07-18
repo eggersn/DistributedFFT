a9d5ae799b48af5fe4e63d66e1eea4a446930605
eggersn <st160727@stud.uni-stuttgart.de>
2021-07-18 11:08:01 +0200

    job sizes

--- evaluation/slab/slab.py
+++ evaluation/slab/slab.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 comm_methods = {"Peer2Peer": 0, "All2All": 1}
 send_methods = [{"Sync": 0, "Streams": 1, "MPI_Type": 2}, {"Sync": 0, "MPI_Type": 2}]
 
-prefix = "../../benchmarks_krypton/slab_z_then_yx"
-opt = 1
+prefix = "../../benchmarks_temp/slab_default"
+opt = 0
 P = 4
-cuda_aware = False
+cuda_aware = True
 prec = "double"
 
 title = "Slab Decomposition (Krypton)"
@@ -19,37 +19,51 @@ count = 0
 labels = []
 legend_labels = []
 
+sizes = ["256_256_256", "512_256_256", "256_512_256", "256_256_512"]
+
 with open('out.csv', mode='w') as out_file:
     writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    writer.writerow(["Comm_Method", "Send_Method", 32, 64, 128, 256, 512])
+    writer.writerow(["Comm_Method", "Send_Method"] + sizes)
 
     for comm1 in comm_methods:
         for snd1 in send_methods[comm_methods[comm1]]:
-            files = [f for f in listdir(prefix) if isfile(join(prefix, f)) and re.match("test_{}_{}_{}_\d*_{}_{}".format(opt, comm_methods[comm1], send_methods[comm_methods[comm1]][snd1], (1 if cuda_aware else 0), P), f)]
+            files = [f for f in listdir(prefix) if isfile(join(prefix, f)) and re.match("test_{}_{}_{}_\d*_\d*_\d*_{}_{}".format(opt, comm_methods[comm1], send_methods[comm_methods[comm1]][snd1], (1 if cuda_aware else 0), P), f)]
             
-            x_vals = [int(f.split("_")[4]) for f in files]
-            y_vals = [0 for x in x_vals]
+            x_vals = sizes
+            mean_vals = [0 for x in x_vals]
+            sd_vals = [0 for x in x_vals]
             f_count = 0
             for f in files:
+                for i in range(0, len(sizes)):
+                    if f.split("_")[4] + "_" + f.split("_")[5] + "_" + f.split("_")[6] == sizes[i]:
+                        f_count = i
+
                 with open(join(prefix, f)) as csv_file:
                     csv_reader = csv.reader(csv_file, delimiter=',')
                     line_counter = 0
                     for row in csv_reader:
                         if len(row) > 0 and row[0] == "Run complete":
-                            y_vals[f_count] += sum([float(x) for x in row[1:-1]])
+                            mean_vals[f_count] += sum([float(x) for x in row[1:-1]])
                             line_counter += 1
-                    y_vals[f_count] /= (line_counter*P)
-                    f_count += 1
+                    mean_vals[f_count] /= (line_counter*P)
+                    
+                with open(join(prefix, f)) as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+                    line_counter = 0
+                    for row in csv_reader:
+                        if len(row) > 0 and row[0] == "Run complete":
+                            sd_vals[f_count] += (mean_vals[f_count]-sum([float(x) for x in row[1:-1]])/P)**2
+                            line_counter += 1
+                    sd_vals[f_count] /= (line_counter-1)
 
-            y_vals = [x for _, x in sorted(zip(x_vals, y_vals), key=lambda pair: pair[0])]
-            x_vals = sorted(x_vals)
-            writer.writerow([str(comm1), str(snd1)] + y_vals)
+            writer.writerow([str(comm1), str(snd1)] + mean_vals)
 
             print(count)
             print(comm1 + " " + snd1)
             print(x_vals)
-            print(y_vals)
+            print(mean_vals)
+            print(sd_vals)
             print()
 
             count += 1
--- temp.py
+++ temp.py
@@ -1,12 +1,17 @@
 import numpy as np
-L = 0
-W = 3**9
+# L = 0
+# W = 3**9
 
-P = range(1, 500)
-N = [2**k for k in range(5, 14)]
+# P = range(1, 500)
+# N = [2**k for k in range(5, 14)]
 
-for p in P:
-    for n in N:
-        duration_slab = 1-1/p**2
-        duration_pencil = 2*(1-1/p)
-        print("{}, {}: {} {}".format(p, n, duration_slab, duration_pencil))
+# for p in P:
+#     for n in N:
+#         duration_slab = 1-1/p**2
+#         duration_pencil = 2*(1-1/p)
+#         print("{}, {}: {} {}".format(p, n, duration_slab, duration_pencil))
+
+for p1 in range(2, 29):
+    for p2 in range(2, 29):
+        if p1 * p2 <= 56:
+            print("({}, {})".format(p1, p2))
